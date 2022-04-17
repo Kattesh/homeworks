ecd60f13a21f4b93ba3189ad46a7141c9519cea0
Kate.sh <kate.sh@list.ru>
2022-04-17 13:28:45 -0500

    9HW

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './Clock.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
@@ -7,40 +8,43 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+      clearInterval(timerId)//Функция clearInterval останавливает таймер, заданный функцией setInterval. принимает идентификатор того таймера, который нужно остановить.
     }
     const start = () => {
         stop()
-        const id: number = window.setInterval(() => {
-            // setDate
+        const id: number = +setInterval(() => {
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime =date?.toLocaleTimeString()||<br/>
+    const stringDate =date?.toLocaleDateString()||<br/>
 
     return (
         <div>
-            <div
+            <div className={s.clock}
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
                 {stringTime}
             </div>
 
-            {show && (
-                <div>
+            {show ?  (
+                <div className={s.clock}>
                     {stringDate}
                 </div>
-            )}
+            ):(
+                <div  className={s.clock} />
+                )
+            }
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Clock from './Clock'
 
+
 function HW9() {
     return (
         <div>
