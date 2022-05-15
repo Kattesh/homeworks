c9bfe52b1a4ee15e48c0f4f07fe6193b30c8471e
Kate.sh <kate.sh@list.ru>
2022-05-14 20:31:38 -0500

    11HW

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,28 +1,35 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import s from "./HW11.module.css"
 
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
+    const onDoubleRangeChange = (value: [number, number]) => {
+        setValue1(value[0]);
+        setValue2(value[1]);
+    }
     return (
         <div>
             <hr/>
             homeworks 11
 
             {/*should work (должно работать)*/}
-            <div>
+            <div className={`${s.rangeWrap} ${s.singleRangeWrap}`}>
                 <span>{value1}</span>
                 <SuperRange
-                    // сделать так чтоб value1 изменялось
+                    value={value1}
+                    onChangeRange={setValue1}
                 />
             </div>
 
-            <div>
+            <div className={`${s.rangeWrap} ${s.doubleRangeWrap}`}>
                 <span>{value1}</span>
                 <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
+                    value={[value1, value2]}
+                    onChangeRange={onDoubleRangeChange}
                 />
                 <span>{value2}</span>
             </div>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,76 @@
-import React from 'react'
+import React, {ChangeEvent, useEffect, useState} from 'react'
+import s from "./SuperDoubleRange.module.css";
 
 type SuperDoubleRangePropsType = {
     onChangeRange?: (value: [number, number]) => void
     value?: [number, number]
-    // min, max, step, disable, ...
+    min?:number
+    max?:number
+    step?:number
+    disabled?:boolean
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
-        // min, max, step, disable, ...
+        min, max, step, disabled
     }
 ) => {
-    // сделать самому, можно подключать библиотеки
+
+    const initMin = min ? min : 0;
+    const initMax = max ? max : 100;
+    const stepValue = step ? step : 1;
+    const initMinValue = value ? value[0] : initMin;
+    const initMaxValue = value ? value[1] : initMax;
+
+    const [minValue, setMinValue] = useState(initMinValue);
+    const [maxValue, setMaxValue] = useState(initMaxValue);
+
+    const onMinValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = Math.min(+e.currentTarget.value, maxValue);
+        setMinValue(value);
+        onChangeRange && onChangeRange([value, maxValue]);
+    };
+
+    const onMaxValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = Math.max(+e.currentTarget.value, minValue);
+        setMaxValue(value);
+        onChangeRange && onChangeRange([minValue, value]);
+    };
+
+    useEffect(() => {
+        if (initMinValue <= maxValue && !disabled) setMinValue(initMinValue);
+    }, [initMinValue, maxValue, disabled]);
+
+
 
     return (
-        <>
-            DoubleRange
-        </>
-    )
+        <div className={s.doubleRange}>
+            <div className={s.doubleRangeTrack}/>
+            <input
+                type={"range"}
+                value={minValue}
+                onChange={onMinValueChange}
+                className={s.doubleRangeThumb}
+                // style={{zIndex: minValue === maxValue ? 1 : 0}}
+                style={{zIndex: maxValue === minValue ? 0 : 1}}
+                min={initMin}
+                max={initMax}
+                step={stepValue}
+                disabled={disabled}
+            />
+            <input
+                type={"range"}
+                value={maxValue}
+                onChange={onMaxValueChange}
+                className={`${s.doubleRangeThumb} ${s.doubleRangeRightThumb}`}
+                min={initMin}
+                max={initMax}
+                step={stepValue}
+                disabled={disabled}
+            />
+        </div>
+    );
 }
 
-export default SuperDoubleRange
+export default SuperDoubleRange;
